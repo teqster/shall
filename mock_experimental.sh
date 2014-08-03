704444fb0f4ab7e8d3ac86e86b6870238fc2755f
teqster <teqster@gmail.com>
2014-08-03 15:23:40 +0200

    adding some mocking experiments

---
+++ mock_experimental.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+. ./shall
+
+parseRepoURL()
+{
+  curl https://api.github.com/users/teqster/repos 2>/dev/null|awk '/"html_url":/ {print $2}'|tr -d \"
+}
+
+# defining to how to mock curl
+# TODO: mock individual commands for individual tests (instead of globally mocking them)
+mock_curl()
+{
+  cat<<EOJ
+[
+  {
+    "html_url": "https://github.com/teqster"
+  }
+]
+EOJ
+}
+
+test_repoUrlParse()
+{
+  # unfortunately we cannot go for
+  #    assertOK parseRepoURL|grep -q https://github.com/teqster
+  # here, because the pipe gets interpreted.
+  # maybe something like
+  #    assertOK 'parseRepoURL|grep -q https://github.com/teqster'
+  # and eval later...
+  url=$(parseRepoURL|grep -q https://github.com/teqster)
+  assertOK test -n "$url"
+}
+
+test_repoUrlParse2()
+{
+  url=$(parseRepoURL|grep -q https://github.com/teqster/foo)
+  assertFail test -n "$url"
+}
+
+testRun
